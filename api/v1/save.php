736e96deffa6d412caec82aca8a9845556dd2add
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-24 12:45:20 -0400

    Proper API headers when managing data and files

--- api/v1/save.php
+++ api/v1/save.php
@@ -1,6 +1,11 @@
 <?php
 // Save .scpl file with new contents
 require( "../request.php" );
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 if ( $auth === true ) {
 	$file_id = $_POST[ 'id' ];
 	$content = $_POST[ 'contents' ];
