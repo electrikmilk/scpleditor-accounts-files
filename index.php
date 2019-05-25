d97c508356872843b7a47e975837c8438aece29e
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-25 18:28:55 -0400

    Fixes index resource linking

--- index.php
+++ index.php
@@ -3,25 +3,25 @@
 <html>
 <head>
     <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0"/>
+    <meta name="msapplication-TileColor" content="#1e1f57">
+    <meta name="theme-color" content="#1e1f57">
 
     <?php include("metadata.php"); ?>
 
     <script type="text/javascript" src="//code.jquery.com/jquery-latest.js"></script>
-    <script type="text/javascript" src="main.js"></script>
+    <script type="text/javascript" src="/main.js"></script>
 
-    <link rel="stylesheet" href="main.css"/>
+    <link rel="stylesheet" href="/main.css"/>
 
-    <link rel="shortcut icon" href="favicon/favicon.ico"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0"/>
-    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-    <link rel="manifest" href="favicon/site.webmanifest">
-    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#007aff">
-    <meta name="msapplication-TileColor" content="#1e1f57">
-    <meta name="theme-color" content="#1e1f57">
+    <link rel="shortcut icon" href="/favicon/favicon.ico"/>
 
-    <link rel="manifest" href="favicon/manifest.json"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="manifest" href="/favicon/manifest.json"/>
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#007aff">
 
 </head>
 
