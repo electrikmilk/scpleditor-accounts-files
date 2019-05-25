d8cc802c8b6ce3af2e8019d2a840272be974ac28
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-25 14:19:00 -0400

    Changed 'scpl-files' to 'files'

--- api/v1/create.php
+++ api/v1/create.php
@@ -17,10 +17,10 @@ if ( $auth === true ) {
 		$file_id = randString( 20 );
 		if ( mysqli_query( $connect, "insert into data.files (id,name,type,author) values ('" . $file_id . "','" . $name . "','$type','$id')" ) ) {
 			if ( $type === "file" ) {
-				if ( file_put_contents( "../../scpl-files/$id/$name", $contents ) )echo json_response( "success", "File $name was successfully created." );
+				if ( file_put_contents( "../../files/$id/$name", $contents ) )echo json_response( "success", "File $name was successfully created." );
 				else echo json_response( "error", "Internal file system error creating file $name." );
 			} else {
-				if ( makeFolder( "../../scpl-files/$id/$name" ) )echo json_response( "success", "Folder $name was successfully created." );
+				if ( makeFolder( "../../files/$id/$name" ) )echo json_response( "success", "Folder $name was successfully created." );
 				else echo json_response( "error", "Internal file system error creating folder $name." );
 			}
 		} else echo json_response( "error", "Internal database error creating file $name." );
