0bd889988a7bd78f7c9dd88057ad5b099cfbbabd
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-25 16:59:26 -0400

    If file create successful, returns file 'id'

--- api/v1/create.php
+++ api/v1/create.php
@@ -17,7 +17,10 @@ if ( $auth === true ) {
 		$file_id = randString( 20 );
 		if ( mysqli_query( $connect, "insert into data.files (id,name,type,author) values ('" . $file_id . "','" . $name . "','$type','$id')" ) ) {
 			if ( $type === "file" ) {
-				if ( file_put_contents( "../../files/$id/$name", $contents ) )echo json_response( "success", "File $name was successfully created." );
+				if ( file_put_contents( "../../files/$id/$name", $contents ) ) {
+					$newfile = array("id"=>$file_id);
+					echo json_encode($newfile);
+				}
 				else echo json_response( "error", "Internal file system error creating file $name." );
 			} else {
 				if ( makeFolder( "../../files/$id/$name" ) )echo json_response( "success", "Folder $name was successfully created." );
