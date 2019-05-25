dfb26c21f3dfa02f4e28ca424e485aebf5f8e632
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-25 18:36:23 -0400

    Reset password can now be inline in the account settings page.

--- pages/forgot.php
+++ pages/forgot.php
@@ -16,4 +16,3 @@ if(!$_SESSION) {
 <?php
 }
 ?>
-</form>
--- pages/reset.php
+++ pages/reset.php
@@ -3,22 +3,27 @@ $token = dataArray("tokens",$_GET['token'],"id");
 if(!$_GET['token'] || !$token) {
   //header("Location: /forgot-password");
 }
+if(!$_SESSION)echo '<div class="login-icon"></div>';
 ?>
-<div class="login-icon"></div>
 <h3>Reset Your Password</h3>
 <form action="auth.php" method="POST" >
     <input type="hidden" name="action" value="resetpassword"/>
     <input type="hidden" name="action" value="user_id" value="<?php echo $token['user_id']; ?>"/>
     <input type="hidden" name="action" value="token" value="<?php echo $_GET['token']; ?>"/>
     <div class="mask-btn"></div><input type="text"  id="password" placeholder="New Password" />
-    <br/>
+    <br/><br/>
     <button type="submit" class="primary-btn">Set New Password</button>
 </form>
+<?php
+if(!$_SESSION) {
+?>
 <br/></br/><hr/></br/>
 <div class="login-footer">
-<ul>
-  <li><a href="https://editor.scpl.dev/">Back to Editor</a></li>
-  <li id="signup"><a href="/login">Back to Login</a></li>
-</ul>
+  <ul>
+    <li><a href="https://editor.scpl.dev/">Back to Editor</a></li>
+    <li id="signup"><a href="/login">Back to Login</a></li>
+  </ul>
 </div>
-</form>
+<?php
+}
+?>
