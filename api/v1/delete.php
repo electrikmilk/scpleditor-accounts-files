e9f014d5403d6d26eaf35032cf0fbe64659caa06
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-26 12:01:09 -0400

    Bugs with delete endpoint fixed

--- api/v1/delete.php
+++ api/v1/delete.php
@@ -15,18 +15,19 @@ if ( $auth === true ) {
         $itemdata = dataArray( "files", $file_id, "id" );
         if ( $itemdata ) {
             $name = $itemdata[ 'name' ];
-            $path = dirname( __FILE__ ) . "/$name";
+            $path = dirname( __FILE__ ,3) . "/files/$id/$name";
+            //$path = realpath(__DIR__ . "/../../$name");
             if ( file_exists( $path ) ) {
                 if ( mysqli_query( $connect, "delete from data.files where id = '" . $file_id . "'" ) ) {
                     if ( $type === "file" ) {
                         if ( unlink( $path ) )echo json_response( "success", "File $name was deleted." );
-                        else echo json_response( "error", "There was an internal error deleting $name." );
+                        else echo json_response( "error", "There was an internal file system error deleting file $name." );
                     } else {
                         if ( deleteDir( $path ) )echo json_response( "success", "Folder $name was deleted." );
-                        else echo json_response( "error", "There was an internal error deleting $name." );
+                        else echo json_response( "error", "There was an internal file system error deleting folder $name." );
                     }
-                } else echo json_response( "error", "There was a database error deleting $name." );
-            } else echo json_response( "error", "File does not appear to exist." );
+                } else echo json_response( "error", "There was a internal database error deleting $name." );
+            } else echo json_response( "error", "File at $path does not appear to exist." );
         } else echo json_response( "error", "Invalid file ID." );
     }
 }
