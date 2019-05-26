f3d7a170cd930fb1a5b6dbb13b3e2f8aabfd9000
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-26 00:36:36 -0400

    Responsiveness

--- pages/forgot.php
+++ pages/forgot.php
@@ -9,13 +9,14 @@ if($_SESSION) {
   <p class="subtext">We'll send you a link via email to change the password to your account</p>
   <br/><hr/>
   <?php
+  $class = 'class="width-stretch"';
 } else echo "<h3>Forgot Password</h3>";
 ?>
 <div class="message error" id="forgot-error" name="forgot-password-form" style="display: none;">Error sending reset link.</div>
 <div class="message success" id="forgot-success" style="display: none;">Link sent!</div>
 <form action="auth.php" method="POST" id="forgot-password-form">
     <input type="hidden" name="action" value="sendpasswordlink"/>
-    <input type="text" name="email" id="forgot-email" placeholder="Your Email Address" value="<?php echo $email; ?>" data-require="true" /><br/></br/>
+    <input type="text" name="email" id="forgot-email" placeholder="Your Email Address" <?php echo $class; ?> value="<?php echo $email; ?>" data-require="true" /><br/></br/>
     <button type="submit" class="primary-btn">Send Reset Link</button>
 </form>
 <?php
--- pages/settings.php
+++ pages/settings.php
@@ -9,32 +9,33 @@ if($account['status'] === "0") echo "<div class='message warning'>Your account h
 
 <h3 id="username-title"><?php echo $account['username']; ?></h3>
 <p class="subtext">Account created <?php echo timeago($account['timestamp']); ?></p>
-<p><?php echo file_count("files/".$account['id']); ?></p>
-<?php if($account['updated'])echo "<p>You last updated your account ".timeago($account['updated'])."</p>"; ?>
+<p>You have <?php echo file_count("files/".$account['id']); ?> in your account.</p>
+<?php if($account['updated'])echo "<p>Last updated ".timeago($account['updated'])."</p>"; ?>
 
 <hr/>
 
 <form action="auth.php" method="POST" name="user-settings-form" id="user-settings-form">
   <input type="hidden" name="action" value="updatefields"/>
-  <table class="width-full">
-  <tr>
-    <td class="setting-label-td"><label for="username">Username:</label></td>
-    <td><input type="text" id="username" name="username" class="width-stretch" spellcheck="false" placeholder="Username" value="<?php echo $account['username']; ?>" data-require="true" data-limit="20"/>
-      <div class="input-limit" id="limit-username">
-        0 / 20 character limit
-      </div>
-      <br/>
-    </td>
-  </tr>
-  <tr>
-    <td class="setting-label-td"><label for="email">Email:</label></td>
-    <td><input type="emal" id="email" name="email" class="width-stretch" spellcheck="false" placeholder="Email Address" value="<?php echo $account['email']; ?>" data-require="true"/></td>
-  </tr>
-  <tr>
-    <td class="setting-label-td"><br/><label for="password">Password:</label></td>
-    <td><br/><a href='/forgot-password'>Reset my password</a></td>
-  </tr>
-  </table>
+
+<div class="form-field">
+  <div>Username</div>
+  <div>
+    <input type="text" id="username" name="username" class="width-stretch" spellcheck="false" placeholder="Username" value="<?php echo $account['username']; ?>" data-require="true" data-limit="20"/>
+    <div class="input-limit" id="limit-username">
+      0 / 20 character limit
+    </div>
+  </div>
+</div>
+<div class="form-field">
+  <div>Email Address</div>
+  <div>
+    <input type="emal" id="email" name="email" class="width-stretch" spellcheck="false" placeholder="Email Address" value="<?php echo $account['email']; ?>" data-require="true"/>
+  </div>
+</div>
+<div class="form-field">
+  <div>Password</div>
+  <div><br/><a href='/forgot-password'>Reset my password</a></div>
+</div>
   <br/><br/>
   <button type="submit" class="primary-btn">Save changes</button>
 </form>
