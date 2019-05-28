0a58a69df26ad191b4501d4fb5343feff17c7d41
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-28 12:44:46 -0400

    Added error messages to access endpoint

--- api/v1/access.php
+++ api/v1/access.php
@@ -16,6 +16,7 @@ if ( $auth === true ) {
             $name = $filedata[ 'name' ];
             $owner = $filedata[ 'author' ];
             $loggedin = $token[ 'user_id' ];
+            $collab = explode( ",", $itemdata[ 'collab' ] );
             // Comma seperated list of users that the owner would like to share the file with
             $users = implode( ",", explode( ",", trim( $_POST[ 'users' ] ) ) ); // Reformat/clean list
             $thistype = $itemdata[ 'type' ];
@@ -23,7 +24,9 @@ if ( $auth === true ) {
             if ( $owner === $id ) {
                 if ( mysqli_query( $connect, "update data.files set collab = '$users' where id = '$file_id'" ) )echo json_response( "success", "Collaborators were set for $name." );
                 else echo json_response( "error", "Internal database error setting collaborators." );
-            } else echo json_response( "error", "You do not appear to own that $thetype." );
+            } else if(in_array( $id, $collab ) === true) {
+              echo json_response( "error", "Only the owner of this $thetype is allowed to change who can access it." );
+            } else echo json_response( "error", "You do not own this $thetype." );
         } else echo json_response( "error", "Invalid $thetype ID." );
     }
 }
