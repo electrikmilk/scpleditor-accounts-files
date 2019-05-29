6c2d503ca39a92fd911c0380f35b2df03d54b1f6
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-28 20:30:47 -0400

    Updated API ref, changed /list to /files

--- pages/api.php
+++ pages/api.php
@@ -7,13 +7,13 @@
 
 <div class="endpoint-item">
 <div class="endpoint"><span>/</span>access</div>
-<div class="endpoint-description">This is only a feature for the owner of the file. Send through a comma separated list of user ids to be set as collaborators on said file. Will only ever be files, never folders (for now). This will allow them to only open and save changes to the file.</div>
+<div class="endpoint-description">Endpoint is restricted to the owner of the file. Currently only files may be shared until folder sharing is implemented. This will give the selected collaborators read/write access.</div>
 <div class="endpoint-method">Method: <span>POST</span></div>
 <div class="endpoint-params">
   <h4>URL Params</h4>
   <ul>
     <li>token=[alphanumeric]</li><br/>
-    <li>users=[comma-list-of-ids]</li>
+    <li>users=[comma,list,of,user,ids]</li>
   </ul>
 </div>
 <div class="endpoint-params">
@@ -25,7 +25,7 @@
 </div>
 <div class="endpoint-item">
   <div class="endpoint"><span>/</span>copy</div>
-  <div class="endpoint-description">This is only a feature for the owner of the file or folder. Copy a file or folder set by 'item_id' to a folder set by 'folder_id'. Item ID can be the ID of a folder. If no folder ID is given, the file or folder is copied to the root of their files.</div>
+  <div class="endpoint-description">Endpoint is restricted to the owner of the file. Copy a file or folder set by 'item_id' to a folder set by 'folder_id'. Item ID can be the ID of a folder. If no folder ID is given, the file or folder is copied to the root of their files.</div>
   <div class="endpoint-method">Method: <span>POST</span></div>
   <div class="endpoint-params">
     <h4>URL Params</h4>
@@ -65,7 +65,7 @@
 </div>
 <div class="endpoint-item">
 <div class="endpoint"><span>/</span>delete</div>
-<div class="endpoint-description">This is only a feature for the owner of the file. Delete a file specified by 'id', set type as file or folder.</div>
+<div class="endpoint-description">Endpoint is restricted to the owner of the file. Delete a file specified by 'id', set type as file or folder.</div>
 <div class="endpoint-method">Method: <span>POST</span></div>
 <div class="endpoint-params">
   <h4>URL Params</h4>
@@ -84,7 +84,7 @@
 </div>
 <div class="endpoint-item">
 <div class="endpoint"><span>/</span>file</div>
-<div class="endpoint-description">Get the contents of a file specified by 'id'. This feature is for the owner of a file or a set collaborator.</div>
+<div class="endpoint-description">Get the contents of a file specified by 'id'. Endpoint is restricted to the owner of the file or a set collaborator.</div>
 <div class="endpoint-method">Method: <span>POST</span></div>
 <div class="endpoint-params">
   <h4>URL Params</h4>
@@ -101,8 +101,23 @@
 </div>
 </div>
 <div class="endpoint-item">
+<div class="endpoint"><span>/</span>files</div>
+<div class="endpoint-description">List of current users files (only owned by the user) in JSON format.</div>
+<div class="endpoint-method">Method: <span>POST</span></div>
+<div class="endpoint-params">
+  <h4>URL Params</h4>
+  <ul>
+    <li>token=[alphanumeric]</li>
+  </ul>
+  <h4>Example Response</h4>
+  <ul>
+    <li>[{<br/>"id":"n402n4uc84i20ehf73j8",<br/>"type":"file",<br/>"name":"File.scpl",<br/>"size":"8 KB",<br/>"timestamp":"2019-04-09 15:05:32",<br/>"relativeTimestamp":"Today at 3:05 pm",<br/>"updated":null,<br/>"relativeUpdated":null<br/>},<br/>
+      {<br/>"id":"2ruc944hfkgutiy930du",<br/>"type":"folder",<br/>"name":"Folder",<br/>"size":"0 bytes",<br/>"contents":[...],<br/>"timestamp":"2019-04-09 15:05:32",<br/>"relativeTimestamp":"Today at 3:05 pm",<br/>"updated":null,<br/>"relativeUpdated":null<br/>}]</li>
+  </ul>
+</div>
+<div class="endpoint-item">
 <div class="endpoint"><span>/</span>fork</div>
-<div class="endpoint-description">This is only a feature for a set collaborator of the file, the owner has no reason to do this. Fork a file shared with the current user, specified by 'id' to the root of their user folder.</div>
+<div class="endpoint-description">Endpoint is restricted to file collaborators, owner uses <code>/copy</code>. Fork a file shared with the current user, file is specified by 'id'. File is copied from owners user folder to the root of the current users folder.</div>
 <div class="endpoint-method">Method: <span>POST</span></div>
 <div class="endpoint-params">
   <h4>URL Params</h4>
@@ -118,29 +133,10 @@
   </ul>
 </div>
 </div>
-<div class="endpoint-item">
-<div class="endpoint"><span>/</span>list</div>
-<div class="endpoint-method">Method: <span>POST</span></div>
-<div class="endpoint-params">
-  <h4>URL Params</h4>
-  <ul>
-    <li>token=[alphanumeric]</li>
-  </ul>
-  <h4>Response</h4>
-  <ul>
-    <li>List of current users files (only owned by the user) in JSON format.</li>
-  </ul>
-  <h4>Example Response</h4>
-  <ul>
-    <li>[{<br/>"id":"n402n4uc84i20ehf73j8",<br/>"type":"file",<br/>"name":"File.scpl",<br/>"size":"8 KB",<br/>"timestamp":"2019-04-09 15:05:32",<br/>"relativeTimestamp":"Today at 3:05 pm",<br/>"updated":null,<br/>"relativeUpdated":null<br/>},<br/>
-      {<br/>"id":"2ruc944hfkgutiy930du",<br/>"type":"folder",<br/>"name":"Folder",<br/>"size":"0 bytes",<br/>"contents":[...],<br/>"timestamp":"2019-04-09 15:05:32",<br/>"relativeTimestamp":"Today at 3:05 pm",<br/>"updated":null,<br/>"relativeUpdated":null<br/>}]</li>
-  </ul>
-</div>
-
 </div>
 <div class="endpoint-item">
 <div class="endpoint"><span>/</span>move</div>
-<div class="endpoint-description">This is only a feature for the owner of the file or folder. Move a file or folder specified by 'item_id' to a folder set by 'folder_id'. Item ID can be the ID of a folder. If no folder ID is given, the file or folder is moved to the root of their files.</div>
+<div class="endpoint-description">Endpoint is restricted to the owner of the file or folder. Move a file or folder specified by 'item_id' to a folder set by 'folder_id'. Item ID can be the ID of a folder or file. If no folder ID is given, the file or folder is moved to the root of the current users files.</div>
 <div class="endpoint-method">Method: <span>POST</span></div>
 <div class="endpoint-params">
   <h4>URL Params</h4>
@@ -159,26 +155,26 @@
 </div>
 <div class="endpoint-item">
 <div class="endpoint"><span>/</span>rename</div>
-<div class="endpoint-description">This is only a feature for the owner of the file. Rename a file or folder specified by 'id'. 'name' is the new name of the file or folder.</div>
+<div class="endpoint-description">Endpoint is restricted to the owner of the file or folder. Rename a file or folder specified by 'id'. 'name' is the new name of the file or folder.</div>
 <div class="endpoint-method">Method: <span>POST</span></div>
 <div class="endpoint-params">
   <h4>URL Params</h4>
   <ul>
     <li>token=[alphanumeric]</li><br/>
     <li>id=[alphanumeric]</li><br/>
-    <li>name=[New name to set]</li>
+    <li>name=[new-name]</li>
   </ul>
 </div>
 <div class="endpoint-params">
   <h4>Response</h4>
   <ul>
-    <li>name=[new filename] (special characters removed)</li>
+    <li>name=[new-name] (special characters removed)</li>
   </ul>
 </div>
 </div>
 <div class="endpoint-item">
 <div class="endpoint"><span>/</span>save</div>
-<div class="endpoint-description">Save new contents for a file specified by 'id'. This feature is for the owner of a file or a set collaborator.</div>
+<div class="endpoint-description">Save new contents for a file specified by 'id'. Endpoint is restricted to the owner of the file or a set collaborator.</div>
   <div class="endpoint-method">Method: <span>POST</span></div>
   <div class="endpoint-params">
     <h4>URL Params</h4>
@@ -197,7 +193,7 @@
 </div>
 <div class="endpoint-item">
 <div class="endpoint"><span>/</span>shared</div>
-<div class="endpoint-description">Returns a JSON formatted list of files shared with the user. WIll only ever be files, never folders (for now).</div>
+<div class="endpoint-description">Returns a JSON formatted list of files shared with the user. Currently only files may be shared until folder sharing is implemented. Current user has read/write access to returned files. However, current user may not delete, rename, copy, move or change access to any files shared with them.</div>
   <div class="endpoint-method">Method: <span>POST</span></div>
   <div class="endpoint-params">
     <h4>URL Params</h4>
@@ -216,7 +212,7 @@
 </div>
 <div class="endpoint-item">
 <div class="endpoint"><span>/</span>token</div>
-<div class="endpoint-description">Send 'key' supplied by 'login_key' from the accounts backend. Will return the actual token created when the user logged in that can be used to make requests to the Files REST API.</div>
+<div class="endpoint-description">Send 'key' supplied by '?login_key=' from the accounts backend. Will return the actual token created when the user logged in or signed up that can be used to make requests to the REST API.</div>
   <div class="endpoint-method">Method: <span>POST</span></div>
   <div class="endpoint-params">
     <h4>URL Params</h4>
